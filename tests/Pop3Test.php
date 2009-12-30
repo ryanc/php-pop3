9f3d289fe12951dbfdea35bc2c2edffd628ea655
Ryan Cavicchioni <ryan@confabulator.net>
2009-12-30 08:21:22 -0600

    Pop3: Created PHPUnit test case for Pop3 class.

---
+++ tests/Pop3Test.php
@@ -0,0 +1,151 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+require_once '../lib/Pop3.php';
+
+use Mail\Protocol\Pop3;
+use Mail\Protocol\Pop3Exception;
+
+class Pop3Test extends PHPUnit_Framework_TestCase
+{
+	public function testPop3TCPConnection()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tcp' );
+		$this->assertFalse( $pop3->isConnected() );
+		$pop3->connect();
+		$this->assertTrue( $pop3->isConnected() );
+		$pop3->close();
+	}
+
+	public function testPop3SSLConnection()
+	{
+		$pop3 = new Pop3( 'localhost', 995, 'ssl' );
+		$this->assertFalse( $pop3->isConnected() );
+		$pop3->connect();
+		$this->assertTrue( $pop3->isConnected() );
+		$pop3->close();
+	}
+
+	public function testPop3TLSConnection()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tls' );
+		$this->assertFalse( $pop3->isConnected() );
+		$pop3->connect();
+		$this->assertTrue( $pop3->isConnected() );
+		$pop3->close();
+	}
+
+	public function testPop3Authentication()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tls' );
+		$pop3->connect();
+		$pop3->authenticate( 'poptest', 'foobar12' );
+		$pop3->close();
+		$pop3->connect();
+		try {
+			$pop3->authenticate( 'wrong', 'wrong' );
+		}
+		catch ( Pop3Exception $e ) {
+			return;
+		}
+		$pop3->close();
+		$this->fail();
+	}
+
+	public function testPop3CapaCommand()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tls' );
+		$pop3->connect();
+		$this->assertType( 'string', $pop3->getServerCapabilities( 'raw' ) );
+		$this->assertType( 'array', $pop3->getServerCapabilities( 'array' ) );
+		$pop3->authenticate( 'poptest', 'foobar12' );
+		$this->assertType( 'string', $pop3->getServerCapabilities( 'raw' ) );
+		$this->assertType( 'array', $pop3->getServerCapabilities( 'array' ) );
+		$pop3->close();
+	}
+
+	public function testPop3StatCommand()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tls' );
+		$pop3->connect();
+		$pop3->authenticate( 'poptest', 'foobar12' );
+		$this->assertType( 'array', $pop3->status() );
+		$pop3->close();
+	}
+
+	public function testPop3ListCommand()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tls' );
+		$pop3->connect();
+		$pop3->authenticate( 'poptest', 'foobar12' );
+		$this->assertType( 'array', $pop3->listMessages() );
+		$this->assertType( 'array', $pop3->listMessages(1) );
+		$pop3->close();
+	}
+
+	public function testPop3RetrCommand()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tls' );
+		$pop3->connect();
+		$pop3->authenticate( 'poptest', 'foobar12' );
+		$this->assertType( 'string', $pop3->retrieve(1) );
+		$pop3->close();
+	}
+
+	public function testPop3DeleCommand()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tls' );
+		$pop3->connect();
+		$pop3->authenticate( 'poptest', 'foobar12' );
+		$this->assertTrue( $pop3->delete(1) );
+		$pop3->reset();
+		$pop3->close();
+	}
+
+	public function testPop3RsetCommand()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tls' );
+		$pop3->connect();
+		$pop3->authenticate( 'poptest', 'foobar12' );
+		$pop3->delete(1);
+		$this->assertTrue( $pop3->reset() );
+		$pop3->close();
+	}
+
+	public function testPop3NoopCommand()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tls' );
+		$pop3->connect();
+		$pop3->authenticate( 'poptest', 'foobar12' );
+		$this->assertTrue( $pop3->noop() );
+		$pop3->close();
+	}
+
+	public function testPop3TopCommand()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tls' );
+		$pop3->connect();
+		$pop3->authenticate( 'poptest', 'foobar12' );
+		$this->assertType( 'string', $pop3->top(1) );
+		$pop3->close();
+	}
+
+	public function testPop3UidlCommand()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tls' );
+		$pop3->connect();
+		$pop3->authenticate( 'poptest', 'foobar12' );
+		$this->assertType( 'array', $pop3->uidl() );
+		$this->assertType( 'array', $pop3->uidl(1) );
+		$pop3->close();
+	}
+
+	public function testPop3QuitCommand()
+	{
+		$pop3 = new Pop3( 'localhost', 110, 'tls' );
+		$pop3->connect();
+		$pop3->authenticate( 'poptest', 'foobar12' );
+		$this->assertTrue( $pop3->quit() );
+		$pop3->close();
+	}
+}
+?>
