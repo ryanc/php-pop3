47ac4ce189089ced8c299aad1e72debf71aa0312
Ryan Cavicchioni <ryan@confabulator.net>
2010-07-01 11:38:48 -0500

    Bugfix: Include Log class so the SmtpTest passes.

--- lib/Smtp.php
+++ lib/Smtp.php
@@ -2,3 +2,7 @@
 require_once __DIR__ . '/Connection.php';
 require_once __DIR__ . '/Smtp/Protocol.php';
 require_once __DIR__ . '/Smtp/Exception.php';
+require_once __DIR__ . '/Log.php';
+
+$log = Log::singleton('./log/pop3.log');
+$log->open();
