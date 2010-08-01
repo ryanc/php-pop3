d7c9d469ec0ffe7efc70ae639532c83b4fc62310
Ryan Cavicchioni <ryan@confabulator.net>
2010-08-01 17:19:39 -0500

    Add configuration file for the unit tests (TestConfiguration.php.dist).
    Use this for the connection parameters, usernames and passwords.

--- tests/Pop3Test.php
+++ tests/Pop3Test.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'PHPUnit/Framework.php';
 require_once 'lib/Pop3.php';
+require_once 'tests/TestConfiguration.php';
 
 use Mail\Protocol\Pop3;
 use Mail\Protocol\Pop3_Exception;
@@ -11,7 +12,7 @@ class Pop3Test extends PHPUnit_Framework_TestCase
 
 	public function setUp()
 	{
-		$this->_connection = new Pop3('localhost', 110, 'tls');
+		$this->_connection = new Pop3(TESTS_MAIL_POP3_HOST, TESTS_MAIL_POP3_PORT, 'tls');
 		if ($this->_connection->isConnected() === false) {
 			$this->_connection->connect();
 		}
@@ -26,7 +27,7 @@ class Pop3Test extends PHPUnit_Framework_TestCase
 
 	public function testPop3TCPConnection()
 	{
-		$this->_connection = new Pop3('localhost', 110, 'tcp');
+		$this->_connection = new Pop3(TESTS_MAIL_POP3_HOST, TESTS_MAIL_POP3_PORT, 'tcp');
 		$this->assertFalse($this->_connection->isConnected());
 		$this->_connection->connect();
 		$this->assertTrue($this->_connection->isConnected());
@@ -35,7 +36,7 @@ class Pop3Test extends PHPUnit_Framework_TestCase
 
 	public function testPop3SSLConnection()
 	{
-		$this->_connection = new Pop3('localhost', 995, 'ssl');
+		$this->_connection = new Pop3(TESTS_MAIL_POP3_HOST, TESTS_MAIL_POP3_SSL_PORT, 'ssl');
 		$this->assertFalse($this->_connection->isConnected());
 		$this->_connection->connect();
 		$this->assertTrue($this->_connection->isConnected());
@@ -44,7 +45,7 @@ class Pop3Test extends PHPUnit_Framework_TestCase
 
 	public function testPop3TLSConnection()
 	{
-		$this->_connection = new Pop3('localhost', 110, 'tls');
+		$this->_connection = new Pop3(TESTS_MAIL_POP3_HOST, TESTS_MAIL_POP3_PORT, 'tls');
 		$this->assertFalse($this->_connection->isConnected());
 		$this->_connection->connect();
 		$this->assertTrue($this->_connection->isConnected());
@@ -53,7 +54,7 @@ class Pop3Test extends PHPUnit_Framework_TestCase
 
 	public function testPop3AuthPlain()
 	{
-		$this->assertTrue($this->_connection->authenticate('poptest', 'foobar12', 'plain'));
+		$this->assertTrue($this->_connection->authenticate(TESTS_MAIL_POP3_USER, TESTS_MAIL_POP3_PASSWORD, 'plain'));
 		/*
 		$this->_connection->connect();
 		try {
@@ -69,7 +70,7 @@ class Pop3Test extends PHPUnit_Framework_TestCase
 
 	public function testPop3AuthLogin()
 	{
-		$this->assertTrue($this->_connection->authenticate('poptest', 'foobar12', 'login'));
+		$this->assertTrue($this->_connection->authenticate(TESTS_MAIL_POP3_USER, TESTS_MAIL_POP3_PASSWORD, 'login'));
 		/*
 		$this->_connection->connect();
 		try {
@@ -87,66 +88,66 @@ class Pop3Test extends PHPUnit_Framework_TestCase
 	{
 		$this->assertType('string', $this->_connection->getServerCapabilities('raw'));
 		$this->assertType('array', $this->_connection->getServerCapabilities('array'));
-		$this->_connection->authenticate('poptest', 'foobar12');
+		$this->_connection->authenticate(TESTS_MAIL_POP3_USER, TESTS_MAIL_POP3_PASSWORD);
 		$this->assertType('string', $this->_connection->getServerCapabilities('raw'));
 		$this->assertType('array', $this->_connection->getServerCapabilities('array'));
 	}
 
 	public function testPop3StatCommand()
 	{
-		$this->_connection->authenticate('poptest', 'foobar12');
+		$this->_connection->authenticate(TESTS_MAIL_POP3_USER, TESTS_MAIL_POP3_PASSWORD);
 		$this->assertType('array', $this->_connection->status());
 	}
 
 	public function testPop3ListCommand()
 	{
-		$this->_connection->authenticate('poptest', 'foobar12');
+		$this->_connection->authenticate(TESTS_MAIL_POP3_USER, TESTS_MAIL_POP3_PASSWORD);
 		$this->assertType('array', $this->_connection->listMessages());
 		$this->assertType('array', $this->_connection->listMessages(1));
 	}
 
 	public function testPop3RetrCommand()
 	{
-		$this->_connection->authenticate('poptest', 'foobar12');
+		$this->_connection->authenticate(TESTS_MAIL_POP3_USER, TESTS_MAIL_POP3_PASSWORD);
 		$this->assertType('string', $this->_connection->retrieve(1));
 	}
 
 	public function testPop3DeleCommand()
 	{
-		$this->_connection->authenticate('poptest', 'foobar12');
+		$this->_connection->authenticate(TESTS_MAIL_POP3_USER, TESTS_MAIL_POP3_PASSWORD);
 		$this->assertTrue($this->_connection->delete(1));
 		$this->_connection->reset();
 	}
 
 	public function testPop3RsetCommand()
 	{
-		$this->_connection->authenticate('poptest', 'foobar12');
+		$this->_connection->authenticate(TESTS_MAIL_POP3_USER, TESTS_MAIL_POP3_PASSWORD);
 		$this->_connection->delete(1);
 		$this->assertTrue($this->_connection->reset());
 	}
 
 	public function testPop3NoopCommand()
 	{
-		$this->_connection->authenticate('poptest', 'foobar12');
+		$this->_connection->authenticate(TESTS_MAIL_POP3_USER, TESTS_MAIL_POP3_PASSWORD);
 		$this->assertTrue($this->_connection->noop());
 	}
 
 	public function testPop3TopCommand()
 	{
-		$this->_connection->authenticate('poptest', 'foobar12');
+		$this->_connection->authenticate(TESTS_MAIL_POP3_USER, TESTS_MAIL_POP3_PASSWORD);
 		$this->assertType('string', $this->_connection->top(1));
 	}
 
 	public function testPop3UidlCommand()
 	{
-		$this->_connection->authenticate('poptest', 'foobar12');
+		$this->_connection->authenticate(TESTS_MAIL_POP3_USER, TESTS_MAIL_POP3_PASSWORD);
 		$this->assertType('array', $this->_connection->uidl());
 		$this->assertType('array', $this->_connection->uidl(1));
 	}
 
 	public function testPop3QuitCommand()
 	{
-		$this->_connection->authenticate('poptest', 'foobar12');
+		$this->_connection->authenticate(TESTS_MAIL_POP3_USER, TESTS_MAIL_POP3_PASSWORD);
 		$this->assertTrue($this->_connection->quit());
 	}
 }
